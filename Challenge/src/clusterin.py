2fdc35a5baaad135af014de76fa5bc01d210c9d9
ga36wab <jan.doerr@tum.de>
2024-09-25 14:35:40 +0200

    changes

--- Challenge/src/clusterin.py
+++ Challenge/src/clusterin.py
@@ -2,3 +2,8 @@ def cluster_savings(problem, routes):
     """ improve route based on savings if combining or cluster before"""
 
     return
+
+def remove_drivers(problem, routes):
+    """ remove drivers if possible"""
+
+    return
--- Challenge/src/main.py
+++ Challenge/src/main.py
@@ -20,12 +20,32 @@ for instance in all_instances:
     # print(problem.couriers)
     # print(problem.travel_times)
     # print(problem.solution.routes)
-    print(problem.solution.objective)
-    print(problem.solution.eval())
-    print(problem.solution.eval())
-    problem.solution.save_to_csv(solution_folder)
+    # print(problem.solution.objective)
+    # print(problem.solution.eval())
+    # print(problem.solution.eval())
+    # problem.solution.save_to_csv(solution_folder)
+    # print(sum([1 for i in problem.solution.routes.values() if len(i) == 2]))
+    # print(sum([1 for i in problem.solution.routes.values() if len(i) > 2]))
+
+    sorted_id = [x for x in sorted(problem.deliveries, key=lambda x: x.pickup_stacking_id)]
+
+    # only show first set of identical pickup stacking id with more than one
+
+    for i, sid in enumerate(sorted_id):
+        if i == len(sorted_id)-1:
+            break
+        if sorted_id[i+1].pickup_stacking_id == sid.pickup_stacking_id:
+            print(sid)
+            print(sorted_id[i+1])
+
+            break
+
+
+
+    sorted_id = []
+
 
 # print([[len(i["couriers"]), len(i['deliveries'])] for i in all_instances])
 
 # check for feasibility
-feasibility_checker.check_feasibility_files(instance_folder, solution_folder)
+# feasibility_checker.check_feasibility_files(instance_folder, solution_folder)
