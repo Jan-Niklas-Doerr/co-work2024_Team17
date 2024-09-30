a5e6520bd33106bbe21040650c69f4c736d60c8b
Jan-Niklas Dörr <jan.doerr@tum.de>
2024-09-30 18:58:42 +0200

    run algo

--- Challenge/src/main.py
+++ Challenge/src/main.py
@@ -16,7 +16,6 @@ solution_folder = pathlib.Path(__file__).parent.parent / 'solutions'
 all_instances = read_data.process_all_instances(instance_folder)
 
 count = 0
-count_fixed = 0
 
 for instance in all_instances:
     problem = Problem(instance)
@@ -26,15 +25,12 @@ for instance in all_instances:
     # print(problem.solution.routes)
     print(problem.solution.objective)
     # problem.solution.improve_matching(N=1000)
-    if not problem.solution.feasible:
-        print('trying to repair')
-        problem.solution.improve_matching(N=500)
-        a = problem.solution.eval()
-        if not a[2]:
-            print('solution is infeasible')
-            count += 1
-        else:
-            count_fixed += 1
+    
+    problem.solution.improve_matching(N=500)
+    a = problem.solution.eval()
+    if not a[2]:
+        print('solution is infeasible')
+        count += 1
 
 
     # print(problem.solution.eval())
@@ -43,7 +39,6 @@ for instance in all_instances:
     # print(sum([1 for i in problem.solution.routes.values() if len(i) > 2]))
 
 print(f'Number of infeasible solutions: {count}')
-print(f'Number of fixed solutions: {count_fixed}')
 # print([[len(i["couriers"]), len(i['deliveries'])] for i in all_instances])
 
 # check for feasibility
